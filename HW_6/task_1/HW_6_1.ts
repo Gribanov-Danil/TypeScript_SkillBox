234e1cce5d0a18c994b8be1e98556b82ac49eb5a
Danil Gribanov <gribanovwork@yandex.ru>
2022-12-06 07:56:46 +0500

    Сделал 1 задание в 6 практике

---
+++ HW_6/task_1/HW_6_1.ts
@@ -0,0 +1,16 @@
+type MyOmit<T, K> = {
+    [P in Exclude<keyof T, K>]: T[P];
+}
+
+
+interface Todo {
+    title: string
+    description: string
+    completed: boolean
+  }
+  
+  type TodoPreview = MyOmit<Todo, 'description' | 'title'>
+  
+  const todo: TodoPreview = {
+    completed: false,
+  }
